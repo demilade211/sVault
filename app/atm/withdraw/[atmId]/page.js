f61514d8f1aca2b56334ae458cb149477e82d3c3
demilade211 <afolabiogunbanwo@gmail.com>
2024-02-10 15:23:32 +0100

    updated for atms created friday

--- app/atm/withdraw/[atmId]/page.js
+++ app/atm/withdraw/[atmId]/page.js
@@ -19,9 +19,13 @@ import catchErrors from '@/utils/catchErrors';
 import CustomMessage from '@/components/modals/CustomMessage';
 import { hoursLeft } from "@/utils/helpers";
 import NotActive from '@/components/modals/NotActive';
+import { isFriday, isSaturday } from 'date-fns'; // Import date-fns functions to check if the createdAt date is Friday or Saturday
+import Weekend from '@/components/modals/Weekend';
+
 
 const Atm = () => {
 
+    const router = useRouter();
     const params = useParams()
     const { atmId } = params
     const [page, setPage] = useState(0)
@@ -33,6 +37,10 @@ const Atm = () => {
         message: ""
     });
     const [showNotActive, setShowNotActive] = useState({
+        show: false,
+        message: ""
+    });
+    const [showItsWeekend, setShowItsWeekend] = useState({
         show: false,
         message: ""
     });
@@ -49,11 +57,12 @@ const Atm = () => {
         name: "",
         bName: "",
         createdAt: "",
-        hoursLeft:""
+        hoursLeft: ""
     })
 
     const handleFeedClose = () => setShowFeedback((prev) => ({ ...prev, show: false }));
     const handleNotActiveClose = () => setShowNotActive((prev) => ({ ...prev, show: false }));
+    const handleItsWeekend = () => setShowItsWeekend((prev) => ({ ...prev, show: false }));
 
 
     useEffect(() => {
@@ -64,12 +73,21 @@ const Atm = () => {
                 const res2 = await getNameMessage(atmId)
                 setLoading(false)
                 setBanks(res.banks);
-                setAccountInfo(prev => ({ ...prev, bName: res2.name,createdAt:res2.createdAt }))
+                setAccountInfo(prev => ({ ...prev, bName: res2.name, createdAt: res2.createdAt }))
                 setShowFeedback(prev => ({ ...prev, message: res2.customMessage }))
                 const { isWithin24Hours, hoursLefts } = hoursLeft(res2.createdAt)
-                setAccountInfo(prev => ({ ...prev, bName: res2.name,hoursLeft:hoursLefts })) 
+                setAccountInfo(prev => ({ ...prev, bName: res2.name, hoursLeft: hoursLefts }))
                 console.log(isWithin24Hours);
-                isWithin24Hours?setShowNotActive(prev => ({ ...prev, show: true })): setShowFeedback(prev => ({ ...prev, show: true }))
+                if (isWithin24Hours) {
+                    // Check if createdAt date is Friday or Saturday
+                    if (isFriday(res2.createdAt) || isSaturday(res2.createdAt)) {
+                        setShowItsWeekend(prev => ({ ...prev, show: true }))
+                    } else {
+                        setShowNotActive(prev => ({ ...prev, show: true })); // Set showNotActive to true
+                    }
+                } else {
+                    setShowFeedback(prev => ({ ...prev, show: true })); // Set showFeedback to true
+                }
             } catch (error) {
                 setLoading(false)
                 setSnackInfo(prev => ({ ...prev, openSnack: true, type: "error", message: catchErrors(error) }))
@@ -138,7 +156,7 @@ const Atm = () => {
             setLoading(false);
             setSnackInfo(prev => ({ ...prev, openSnack: true, type: "error", message: catchErrors(error) }))
         }
-    } 
+    }
 
     const handleAtmButtons = async (type) => {
         if (page === 0) {
@@ -190,12 +208,21 @@ const Atm = () => {
 
         }
 
+        if (type === "create") {
+            router.push(`/auth/register`)
+        }
+
+        if (type === "try") {
+            router.push(`/`)
+        }
+
     }
 
     return (
         <AtmLayout>
             <Con>
-                <NotActive mOpen={showNotActive.show} handleModClose={handleNotActiveClose} name={accountInfo.bName} hoursLeft={accountInfo.hoursLeft}/>
+                <Weekend mOpen={showItsWeekend.show} handleModClose={handleItsWeekend} name={accountInfo.bName} hoursLeft={accountInfo.hoursLeft} />
+                <NotActive mOpen={showNotActive.show} handleModClose={handleNotActiveClose} name={accountInfo.bName} hoursLeft={accountInfo.hoursLeft} />
                 <CustomMessage mOpen={showFeedback.show} handleModClose={handleFeedClose} message={showFeedback.message} name={accountInfo.bName} />
                 <MySnackBar setSnackInfo={setSnackInfo} snackInfo={snackInfo} />
                 <Screen>
@@ -229,8 +256,8 @@ const Atm = () => {
                     <button className='key' onClick={() => handleAtmButtons(6)}>
                         <p>6</p>
                     </button>
-                    <button className='key yellow'>
-                        <p>Cancel</p>
+                    <button className='key yellow' onClick={() => handleAtmButtons("create")}>
+                        <p>Create Suprise</p>
                     </button>
                     <button className='key' onClick={() => handleAtmButtons(7)}>
                         <p>7</p>
@@ -241,8 +268,8 @@ const Atm = () => {
                     <button className='key' onClick={() => handleAtmButtons(9)}>
                         <p>9</p>
                     </button>
-                    <button className='key'>
-                        <p></p>
+                    <button className='key try' onClick={() => handleAtmButtons("try")}>
+                        <p>Try it Now</p>
                     </button>
                     <button className='key'>
                         <p></p>
@@ -325,6 +352,11 @@ const KeysCon = styled.div`
             background: rgba(0, 255, 102, 1); 
             p{
                 color: rgba(73, 73, 73, 1);
+                font-size: 14px;
+            }
+        }
+        &.try{ 
+            p{ 
                 font-size: 14px;
             }
         }
